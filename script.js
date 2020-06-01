90f7a19beb25089a4e582bc2c0549060d08de86e
DevHSA <harishsa85@gmail.com>
2020-06-01 17:50:26 +0530

    First commit for react

---
+++ script.js
@@ -0,0 +1,23 @@
+// document.addEventListener('DOMContentLoaded', () => {
+
+// })
+
+
+//LESSON 1 LET AND CONST
+// const a =5
+// console.log(a)
+// a = 6
+// console.log(a)
+
+//LESSON 2 ARROW FUNC
+function print(name){
+    console.log(name);
+}
+
+print('something');
+
+const print1 = name => {
+    console.log(name);
+}
+
+print1('onevar')
