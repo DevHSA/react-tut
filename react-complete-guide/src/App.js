6cc175d42b7f77cde04b4768d8342794bb129626
DevHSA <harishsa85@gmail.com>
2020-06-02 16:49:47 +0530

    Events Started

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,17 +4,31 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons: [
+
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 28 },
+      { name: 'Stephanie', age: 28 },
+
+    ]
+  }
+
+  switchNameHandler
+
   render() {
-
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a react App'))
-
     return (
+
       <div className="App">
         <h1>Hi, I'm a react App</h1>
-        <p>This is really working</p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29" > My hobbies: Racing </Person>
-        <Person name="Stephanie" age="26" />
+        <p>This is really working!</p>
+        <button onClick={}>SwitchName</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My hobbies: Racing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+
       </div>
       
     );
