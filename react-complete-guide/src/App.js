3b4ce9289414558faf1139b74860b0cbd44c7608
DevHSA <harishsa85@gmail.com>
2020-06-02 18:36:45 +0530

    Incomplete Hooks

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,38 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
+const app = props => {
 
-  state = {
+  const [ personsState, setPersonsState ] = useState({
+    
     persons: [
-
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 28 },
       { name: 'Stephanie', age: 28 },
+    ],
+    OtherState: 'Some Other Value'
+  });
 
-    ]
-  }
+  const switchNameHandler = () => {
 
-  switchNameHandler
+    console.log("Was Clicked");
+    //DONT DO THIS :P this.state.persons[0].name = 'Harish'
+    this.setPersonsState({
+      persons: [
+  
+        { name: 'Harish', age: 28 },
+        { name: 'Manu', age: 28 },
+        { name: 'Stephanie', age: 26 },
+  
+      ]
+    });
 
-  render() {
+  // render() {
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a react App'))
     return (
 
       <div className="App">
         <h1>Hi, I'm a react App</h1>
         <p>This is really working!</p>
-        <button onClick={}>SwitchName</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My hobbies: Racing </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={switchNameHandler}>SwitchName</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} > My hobbies: Racing </Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
 
       </div>
-      
+
     );
-  }
+  // }
 }
 
-export default App;
+
+}
+
+
+export default app;
