34b57efeeab868d124ceba8aaa854eaa5a0b9b9f
DevHSA <harishsa85@gmail.com>
2020-06-02 13:42:29 +0530

    NEAR Dynamic Content

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  render() {
+
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a react App'))
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react App</h1>
+        <p>This is really working</p>
+        <Person />
+        <Person />
+        <Person />
+      </div>
+      
+    );
+  }
+}
+
+export default App;
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const person = () => {
+
+    return <p>I'm a person and i am X years old!</p>
+}
+
+export default person;
