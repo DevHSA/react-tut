c20e812f22d83cbc84cdd427b002ad32b2e639a8
DevHSA <harishsa85@gmail.com>
2021-03-01 17:17:45 +0530

    Some progress

--- react-complete-guide/src/containers/Orders/Orders.js
+++ react-complete-guide/src/containers/Orders/Orders.js
@@ -1,9 +1,41 @@
 import React from 'react'
-import Order from '../../../components/Order/Order'
-
+import Order from '../../components/Order/Order'
+import axios from '../../axios-orders'
 
 class Orders extends React.Component {
 
+
+    state ={
+
+        orders: [],
+        loading: true,
+    }
+
+    componentDidMount(){
+
+        axios.get('/orders.json').then(res=>{
+
+            let OrderArray = [];
+            for(let key in res.data) {
+                
+                OrderArray.push({
+
+                    ...res.data[key],
+                    id: key
+
+                })
+                
+            }
+
+
+            console.log(res.data);
+            this.setState({loading:false , orders: OrderArray });
+        }).catch(err=>{
+
+            this.setState({loading:false});
+        })
+
+    }
 
     render() {
 
